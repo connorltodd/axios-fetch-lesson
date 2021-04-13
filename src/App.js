ef70b257761ce749cd43084662641be9e95653eb
Connor Todd <connortodd0304@gmail.com>
2021-04-13 15:41:51 +0100

    axios fetch random joke generator completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [randomJoke, setRandomJoke] = React.useState({});
+
+  function getRandomJoke() {
+    axios
+      .get("https://official-joke-api.appspot.com/random_joke")
+      .then(function (response) {
+        // handle success
+        setRandomJoke(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getRandomJoke}>Click me to return a random joke</button>
+      <h1>{randomJoke.setup}</h1>
+      <p>{randomJoke.punchline}</p>
     </div>
   );
 }
